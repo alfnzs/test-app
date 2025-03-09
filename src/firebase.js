5cc9d3153fcfc176ea6325f6533599158b40d57b
alfina zahra <alfinaz916@gmail.com>
2025-03-09 16:50:57 +0700

    add workflow

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import {initializeApp} from "firebase/app"
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDKOW-kAoFjtHVC978Vpyiwrc0XQQAvnMA",
-    authDomain: "test-project-ed184.firebaseapp.com",
-    projectId: "test-project-ed184",
-    storageBucket: "test-project-ed184.firebasestorage.app",
-    messagingSenderId: "876677035894",
-    appId: "1:876677035894:web:9ed177a830a5c2621a6c57"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 export const app=initializeApp(firebaseConfig);
